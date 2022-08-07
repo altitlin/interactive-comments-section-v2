0ae518e079ada1bc73de180cdde75b1479f0f4a2
Alexander Titov <titov.alex627@gmail.com>
2022-08-07 21:41:51 +0300

    feature/ssl: add ssl (#13)
    Co-authored-by: Alexander Titov1 <alexander_titov1@epam.com>

--- fake-api/cors.prod.js
+++ fake-api/cors.prod.js
@@ -1,5 +1,5 @@
 module.exports = (_, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost')
+  res.header('Access-Control-Allow-Origin', 'http://interactive-comments-section.ru')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
   next()
